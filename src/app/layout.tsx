e6fcca4fb4d4796d563eedb3ce0033416daa731f
ThanSoe <89002719+ThanSoe16@users.noreply.github.com>
2025-06-29 21:27:07 +0700

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,11 @@ export default function RootLayout({
           type="image/png"
           sizes="32x32"
         />
+        <meta property="og:image" content="<generated>" />
+        <meta property="og:image:alt" content="About Acme" />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
