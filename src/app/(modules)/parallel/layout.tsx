68b7cee52bc5c6c47323c5b43a3b637f1544e006
ThanSoe <89002719+ThanSoe16@users.noreply.github.com>
2025-06-29 20:18:42 +0700

    RLS validation

--- src/app/(modules)/parallel/layout.tsx
+++ src/app/(modules)/parallel/layout.tsx
@@ -7,11 +7,11 @@ export default function Layout({
   analytics: React.ReactNode;
   team: React.ReactNode;
 }) {
+  const role = "user";
   return (
-    <div className="grid grid-cols-3">
+    <div className="grid grid-cols-2">
       {children}
-      {team}
-      {analytics}
+      {role === "user" ? team : analytics}
     </div>
   );
 }
