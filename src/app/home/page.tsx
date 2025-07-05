9a7b49c9b67d64ac138999c4e8a28114563e7118
ThanSoe <89002719+ThanSoe16@users.noreply.github.com>
2025-07-05 23:23:58 +0700

    error handling

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,8 +1,13 @@
-import Button from "@/components/button";
-import Image from "next/image";
 import { Form } from "./_components/form";
+import { notFound } from "next/navigation";
 
 export default function Home() {
+  const user = false;
+
+  if (user) {
+    notFound();
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
